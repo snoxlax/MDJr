bab5449e4896bc50b37a7413a8558ee57c891ff5
Vlad Grey <Vlad9rey@gmail.com>
2025-11-21 14:26:20 +0200

    Fixed paddings

--- src/ConvertedMd.tsx
+++ src/ConvertedMd.tsx
@@ -1,11 +1,11 @@
 import { useNavigate } from 'react-router-dom';
-import MDToHtml from './MDToHtml'
+import MDToHtml from './MDToHtml';
 import { Button } from 'react-bootstrap';
 import DownloadHtml from './DownloadHtml';
 import { usePDF } from 'react-to-pdf';
 
 interface ConvertedMdProps {
-  markdownContent: string
+  markdownContent: string;
 }
 
 export default function ConvertedMd({ markdownContent }: ConvertedMdProps) {
@@ -17,23 +17,29 @@ export default function ConvertedMd({ markdownContent }: ConvertedMdProps) {
   };
 
   return (
-    <div className='mx-auto p-4' style={{ maxWidth: '90rem' }}>
-
+    <div
+      className="mx-auto py-4"
+      style={{ maxWidth: '90rem' }}
+    >
       <div ref={targetRef}>
-        <MDToHtml>
-          {markdownContent}
-        </MDToHtml>
+        <MDToHtml>{markdownContent}</MDToHtml>
       </div>
-      <div className='d-flex justify-content-between m-4'>
-        <Button variant="outline-primary" onClick={handleClick} className="text-bold">
+      <div className="d-flex justify-content-between p-4">
+        <Button
+          variant="outline-primary"
+          onClick={handleClick}
+          className="text-bold"
+        >
           Back
         </Button>
-        <DownloadHtml variant="outline-primary" className="text-bold" />
+        <DownloadHtml
+          variant="outline-primary"
+          className="text-bold"
+        />
         {/* Button to trigger print */}
 
         {/* <Button onClick={() => toPDF()}>Print to PDF</Button> */}
-
       </div>
-    </div >
-  )
+    </div>
+  );
 }
